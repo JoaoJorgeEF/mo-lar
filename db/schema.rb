10e6c2a520b2dc0bdc5217054e0a1772db4f75e2
João Jorge <52219312+JoaoJorgeEF@users.noreply.github.com>
2022-10-16 18:04:06 -0300

    novos campos ded usuarios

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_16_184410) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_16_205817) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -35,6 +35,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_16_184410) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "nome"
+    t.string "cpf"
+    t.string "telefone"
     t.index ["email"], name: "index_usuarios_on_email", unique: true
     t.index ["reset_password_token"], name: "index_usuarios_on_reset_password_token", unique: true
   end
